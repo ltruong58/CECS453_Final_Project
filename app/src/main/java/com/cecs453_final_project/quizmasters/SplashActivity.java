d165249f1802ffb7e40a8a6b36e6d7bcdb9b9351
Austin Tao <32249785+MrAlphalfa97@users.noreply.github.com>
2019-04-26 01:19:16 -0700

    renaming files: 'SplashActivity.java', 'activity_splash.xml'
    - renaming these files to clarify that this is not the main activity
    - the manifest has also been edited to reflect these changes

--- app/src/main/java/com/cecs453_final_project/quizmasters/SplashActivity.java
+++ app/src/main/java/com/cecs453_final_project/quizmasters/SplashActivity.java
@@ -5,19 +5,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent splashIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent splashIntent = new Intent(SplashActivity.this, RegisterActivity.class);
                 startActivity(splashIntent);
                 finish();
             }
