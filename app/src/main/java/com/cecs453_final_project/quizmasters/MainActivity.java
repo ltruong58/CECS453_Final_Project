a48ee91a4209ab3ed09ec3cd2e073937d95b469f
Austin Tao <32249785+MrAlphalfa97@users.noreply.github.com>
2019-04-26 01:00:34 -0700

    splash screen things
    - Created the splash screen.
    - Created a logo for the splash screen.
    - Added the code that will allow the splash screen to be there, and then move on.

--- app/src/main/java/com/cecs453_final_project/quizmasters/MainActivity.java
+++ app/src/main/java/com/cecs453_final_project/quizmasters/MainActivity.java
@@ -1,13 +1,26 @@
 package com.cecs453_final_project.quizmasters;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
