785f7102a2dec1340aa9f8afb9d30e59b4f8b4c1
Brian <brianjlombardo@gmail.com>
2019-05-03 09:54:24 -0700

    DBHelper update

--- app/src/main/java/com/cecs453_final_project/quizmasters/DBHelper.java
+++ app/src/main/java/com/cecs453_final_project/quizmasters/DBHelper.java
@@ -1,16 +1,17 @@
 package com.cecs453_final_project.quizmasters;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
+import android.database.sqlite.SQLiteDatabase;
 
 public class DBHelper extends SQLiteOpenHelper {
+
+
 
     public static final String DATABASE_NAME = "MyDBName.db";
 
@@ -37,9 +38,9 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String ANSWERS_COLUMN_ANSWERID = "answerID";
     public static final String ANSWERS_COLUMN_QUIZID = "quizID";
     public static final String ANSWERS_COLUMN_QUESTIONID = "questionID";
+    public static final String ANSWERS_COLUMN_CHOSENANSWER = "chosenAnswer";
 
 
-    private HashMap hp;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME , null, 1);
@@ -61,15 +62,17 @@ public class DBHelper extends SQLiteOpenHelper {
 
         db.execSQL(
                 "create table takenQuizzes " +
-                        "(takenQuizID integer primary key, userName text, takenDate text)"
+                        "(takenQuizID integer primary key, accountID integer)"
         );
 
         db.execSQL(
                 "create table answers " +
-                        "(answerID integer primary key, quizID integer, questionID integer ,selectedAnswer text)"
+                        "(answerID integer primary key, quizID integer, questionID integer ,chosenAnswer text)"
         );
 
     }
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -77,7 +80,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS accounts");
         db.execSQL("DROP TABLE IF EXISTS questions");
         db.execSQL("DROP TABLE IF EXISTS takenQuizzes");
-        db.execSQL("DROP TABLE IF EXISTS pickedQuestions");
+        db.execSQL("DROP TABLE IF EXISTS answers");
         onCreate(db);
     }
 
@@ -93,37 +96,36 @@ public class DBHelper extends SQLiteOpenHelper {
         return true;
     }
 
-    public boolean insertQuestion (String questionText, String correctAnswer, String altAnswer1, String altAnswer2, String altAnswer3) {
+    public boolean insertQuestion (String questionText, String correctAnswer, String altAnswer1, String altAnswer2, String altAnswer3, Integer difficulty) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("questionText", questionText);
         contentValues.put("correctAnswer", correctAnswer);
         contentValues.put("altAnswer1", altAnswer1);
-        contentValues.put("altAnswer2", altAnswer1);
-        contentValues.put("altAnswer3", altAnswer1);
+        contentValues.put("altAnswer2", altAnswer2);
+        contentValues.put("altAnswer3", altAnswer3);
+        contentValues.put("difficulty", difficulty);
         db.insert("questions", null, contentValues);
         return true;
     }
 
-    public boolean insertTakenQuiz (String userName, String password, String type) {
+    public int insertTakenQuiz (Integer accountID) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("userName", userName);
-        contentValues.put("password", password);
-        contentValues.put("type", type);
-        db.insert("accounts", null, contentValues);
-        return true;
+        contentValues.put("accountID", accountID);
+        return (int) db.insert("takenQuizzes", null, contentValues);
     }
 
-    public boolean insertPickedQuestion (String userName, String password, String type) {
+    public int insertAnswer (Integer quizID, Integer questionID, String chosenAnswer) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("userName", userName);
-        contentValues.put("password", password);
-        contentValues.put("type", type);
-        db.insert("pickedQuestions", null, contentValues);
-        return true;
+        contentValues.put("quizID", quizID);
+        contentValues.put("questionID", questionID);
+        contentValues.put("chosenAnswer", chosenAnswer);
+        return (int) db.insert("answers", null, contentValues);
     }
+
+
 
     public Integer deleteAccount (Integer id) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -153,37 +155,7 @@ public class DBHelper extends SQLiteOpenHelper {
                 new String[] { Integer.toString(id) });
     }
 
-    public ArrayList<Question> getCertainQuestions(int[] questionIDs) {
 
-        String[] q_id_strings = new String[questionIDs.length];
-        for (int i = 0; i < questionIDs.length; i++) {
-            q_id_strings[i] = String.valueOf(questionIDs[i]);
-        }
-
-        ArrayList<Question> array_list = new ArrayList<Question>();
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from questions where " + QUESTIONS_COLUMN_QUESTIONID + " = ? ", q_id_strings);
-
-        res.moveToFirst();
-        while(res.isAfterLast() == false) {
-            Question q = new Question(
-                    Integer.parseInt(res.getString(res.getColumnIndex(QUESTIONS_COLUMN_QUESTIONID))),
-                    res.getString(res.getColumnIndex(QUESTIONS_COLUMN_QUESTIONTEXT)),
-                    res.getString(res.getColumnIndex(QUESTIONS_COLUMN_CORRECTANSWER)),
-                    res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER1)),
-                    res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER2)),
-                    res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER3)),
-                    2
-            );//Integer.parseInt(res.getString(res.getColumnIndex(QUESTIONS_COLUMN_DIFFICULTY)))
-
-            array_list.add(q);
-
-            res.moveToNext();
-        }
-        res.close();
-
-        return array_list;
-    }
 
     public ArrayList<Question> getAllQuestions() {
         ArrayList<Question> array_list = new ArrayList<Question>();
@@ -201,16 +173,18 @@ public class DBHelper extends SQLiteOpenHelper {
                     res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER1)),
                     res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER2)),
                     res.getString(res.getColumnIndex(QUESTIONS_COLUMN_ALTANSWER3)),
-                    2
+                    res.getInt(res.getColumnIndex(QUESTIONS_COLUMN_DIFFICULTY))
+
             );//Integer.parseInt(res.getString(res.getColumnIndex(QUESTIONS_COLUMN_DIFFICULTY)))
 
             array_list.add(q);
 
             res.moveToNext();
         }
-        res.close();
         return array_list;
     }
+
+
 
     public ArrayList<Question> getFiveQuestions(int difficulty) {
         ArrayList<Question> array_list = new ArrayList<Question>();
@@ -269,31 +243,109 @@ public class DBHelper extends SQLiteOpenHelper {
         return array_list;
     }
 
-    public Cursor getData(int id) {
+
+
+    public Answer getAnswerByID(int aID) {
+        ArrayList<Answer> array_list = new ArrayList<Answer>();
+
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from contacts where id="+id+"", null );
-        return res;
+        Cursor res =  db.rawQuery( "select * from answers where answerID = ?", new String[] {Integer.toString(aID)} );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Answer a = new Answer(
+                    Integer.parseInt(res.getString(res.getColumnIndex(ANSWERS_COLUMN_ANSWERID))),
+                    Integer.parseInt(res.getString(res.getColumnIndex(ANSWERS_COLUMN_QUIZID))),
+                    Integer.parseInt(res.getString(res.getColumnIndex(ANSWERS_COLUMN_QUESTIONID))),
+                    res.getString(res.getColumnIndex(ANSWERS_COLUMN_CHOSENANSWER))
+            );
+
+            array_list.add(a);
+
+            res.moveToNext();
+        }
+        return array_list.get(0);
     }
 
-    public int numberOfRows(){
-        //SQLiteDatabase db = this.getReadableDatabase();
-        //int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
-        //return numRows;
-        return 1;
+
+
+    public boolean usernameAlreadyTaken(String un) {
+        ArrayList<Account> array_list = new ArrayList<Account>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from accounts where username = ?", new String[] {un} );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Account a = new Account(
+                    Integer.parseInt(res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_ACCOUNTID))),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_USERNAME)),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_PASSWORD)),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_TYPE))
+            );
+
+            array_list.add(a);
+
+            res.moveToNext();
+        }
+
+        if( array_list.size()==0 ){
+            return false; //that username is not already in the database
+        }else{
+            return true; //that username is taken
+        }
+
+
     }
 
-    public boolean updateContact (Integer id, String name, String phone, String email, String street,String place) {
+
+
+    public String verifyLogin(String un, String pw) {
+        ArrayList<Account> array_list = new ArrayList<Account>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from accounts where username = ?", new String[] {un} );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Account a = new Account(
+                    Integer.parseInt(res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_ACCOUNTID))),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_USERNAME)),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_PASSWORD)),
+                    res.getString(res.getColumnIndex(ACCOUNTS_COLUMN_TYPE))
+            );
+
+            array_list.add(a);
+
+            res.moveToNext();
+        }
+
+        if( array_list.size()==0 ){
+            return "Username not found"; //if that username is not in the database
+        }else{
+
+
+            if( (array_list.get(0).getPassword()).equals(pw) ){
+                return "Succesfully loged in. AccountID = "+array_list.get(0).getAccountID(); //if that username is in the database and the password is correct
+            }else{
+                return "Incorrect password"; //if that username is in the database but the password is wrong
+            }
+
+
+        }
+
+
+    }
+
+
+
+    public boolean changePassword ( String un, String newpassword ) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("phone", phone);
-        contentValues.put("email", email);
-        contentValues.put("street", street);
-        contentValues.put("place", place);
-        db.update("contacts", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        contentValues.put("password", newpassword);
+        db.update("accounts", contentValues, "username = ? ", new String[] { un } );
         return true;
     }
-
 
 
 
